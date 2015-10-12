360e9c6e8a5c65d8ed8225b7a1edca986659cf65
Eddie Gurnee <pegurnee@gmail.com>
2015-10-12 15:43:46 -0400

    add fresh start option

--- src/edu/emich/power_cosmic/fog/application/DatabaseProject.java
+++ src/edu/emich/power_cosmic/fog/application/DatabaseProject.java
@@ -1,5 +1,6 @@
 package edu.emich.power_cosmic.fog.application;
 
+import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
@@ -22,96 +23,29 @@ import edu.emich.power_cosmic.fog.schema.FogUser;
 import edu.emich.power_cosmic.fog.schema.ForumThread;
 import edu.emich.power_cosmic.fog.schema.Player;
 
-public class DatabaseProject extends Menu {
+public class DatabaseProject
+	extends Menu {
 
-	public static final String FILENAME = "fog.db4";
-	
-	private FogUser user;
-	
-	public static void main(String[] args) {
-		EmbeddedConfiguration config = Db4oEmbedded.newConfiguration();
-		config.common().objectClass(ForumThread.class).cascadeOnUpdate(true);
-		config.common().objectClass(FogUser.class).cascadeOnUpdate(true);
-		
-		ObjectContainer db = Db4oEmbedded.openFile(config, FILENAME);
-		
-//		BasicInstance.init(db);
-		Scanner keyboard = new Scanner(System.in);
-		
-		DatabaseProject mainMenu = new DatabaseProject();
-		LinkedList<Menu> menuStack = new LinkedList<Menu>();
-		menuStack.push(mainMenu);
-		
-		System.out.println("Welcome to fog");
-		OutputConstants.printTitle("Type 'help' for a list of commands");
-		
-		boolean running = true;
-		MenuNavigator menuStatus = null;
-		
-		while(running) {
-			System.out.print(OutputConstants.PROMPT);
-			String command = keyboard.next();
-			String line = keyboard.nextLine().trim();
-			String[] arguments = line.length() > 0? line.split("\\s+") : new String[0];
-			menuStatus = menuStack.peek().doCommand(command, keyboard, db, arguments);
-			
-			switch(menuStatus.getStatus()) {
-			case CHANGE:
-				menuStack.push(menuStatus.getMenu());
-				break;
-			case BACK:
-				menuStack.pop();
-				if (menuStack.isEmpty()) {
-					running = false;
-				} else if (menuStack.size() == 1) {
-					mainMenu.user = null;
-				}
-				break;
-			case LOGOUT:
-				while (menuStack.size() > 1) {
-					menuStack.pop();
-					mainMenu.user = null;
-				}
-				System.out.println("Logged out successfully");
-				break;
-			case EXIT:
-				running = false;
-				break;
-			default:
-				break;
-			}
-		}
-		db.commit();
-		db.close();
-	}
-	
-	public DatabaseProject() {
-		super(null);
-		
-		addCommand(new Login());
-		addCommand(new UserSignup());
-	}
-	
-	private class Login extends Command {
-		
+	private class Login
+		extends Command {
+
 		private Login() {
 			super("login", "Login to the system");
 		}
 
 		@Override
-		public MenuNavigator doCommand(Scanner keyboard, 
-				ObjectContainer db,
+		public MenuNavigator doCommand(Scanner keyboard, ObjectContainer db,
 				String[] args) {
-			
+
 			String username, password;
-			
+
 			if (args.length > 0) {
 				username = args[0];
 			} else {
 				System.out.print("username: ");
 				username = keyboard.nextLine();
 			}
-			
+
 			if (args.length > 1) {
 				password = args[1];
 			} else {
@@ -120,7 +54,8 @@ public class DatabaseProject extends Menu {
 			}
 
 			List<FogUser> users = db.query(new Predicate<FogUser>() {
-				private static final long serialVersionUID = 7591544779191591305L;
+				private static final long serialVersionUID =
+						7591544779191591305L;
 
 				@Override
 				public boolean match(FogUser user) {
@@ -128,29 +63,134 @@ public class DatabaseProject extends Menu {
 							&& user.getPassword().equals(password);
 				}
 			});
-			
+
 			if (users.isEmpty()) {
 				System.out.println("Invalid credentials");
 				return new MenuNavigator(MenuNavigator.Status.CONTINUE);
 			} else {
-				user = users.get(0);
+				DatabaseProject.this.user = users.get(0);
 				MenuNavigator navigator = null;
-				if (user instanceof Player) {
+				if (DatabaseProject.this.user instanceof Player) {
 					navigator = new MenuNavigator(
-							new PlayerMenu((Player)user));
-				} else if (user instanceof Developer) {
-					navigator = new MenuNavigator(
-							new DeveloperMenu((Developer)user));
-				} else if (user instanceof Administrator) {
-					navigator = new MenuNavigator(
-							new AdministratorMenu((Administrator)user));
+							new PlayerMenu((Player) DatabaseProject.this.user));
+				} else if (DatabaseProject.this.user instanceof Developer) {
+					navigator = new MenuNavigator(new DeveloperMenu(
+							(Developer) DatabaseProject.this.user));
+				} else if (DatabaseProject.this.user instanceof Administrator) {
+					navigator = new MenuNavigator(new AdministratorMenu(
+							(Administrator) DatabaseProject.this.user));
 				}
-				
+
 				System.out.println("Login successful");
 				return navigator;
 			}
 		}
 	}
-	
-	
+
+	public static final String FILENAME = "fog.db4";
+
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("begin?");
+		String item = keyboard.nextLine();
+		boolean fresh = false;
+
+		ObjectContainer db;
+
+		switch (item) {
+			case "fresh":
+				final File file = new File(FILENAME);
+				file.delete();
+				fresh = true;
+
+		}
+		EmbeddedConfiguration config = Db4oEmbedded.newConfiguration();
+		config.common().objectClass(ForumThread.class).cascadeOnUpdate(true);
+		config.common().objectClass(FogUser.class).cascadeOnUpdate(true);
+
+		db = Db4oEmbedded.openFile(config, FILENAME);
+
+		if (fresh) {
+			BasicInstance.init(db);
+		}
+
+		DatabaseProject mainMenu = new DatabaseProject();
+		LinkedList<Menu> menuStack = new LinkedList<Menu>();
+		menuStack.push(mainMenu);
+
+		System.out.println("Welcome to fog");
+		OutputConstants.printTitle("Type 'help' for a list of commands");
+
+		boolean running = true;
+		MenuNavigator menuStatus = null;
+
+		while (running) {
+			System.out.print(OutputConstants.PROMPT);
+			String command = interpretCommand(keyboard.next());
+
+			String line = keyboard.nextLine().trim();
+			String[] arguments =
+					line.length() > 0 ? line.split("\\s+") : new String[0];
+			menuStatus = menuStack.peek().doCommand(command, keyboard, db,
+				arguments);
+
+			switch (menuStatus.getStatus()) {
+				case CHANGE:
+					menuStack.push(menuStatus.getMenu());
+					break;
+				case BACK:
+					menuStack.pop();
+					if (menuStack.isEmpty()) {
+						running = false;
+					} else if (menuStack.size() == 1) {
+						mainMenu.user = null;
+					}
+					break;
+				case LOGOUT:
+					while (menuStack.size() > 1) {
+						menuStack.pop();
+						mainMenu.user = null;
+					}
+					System.out.println("Logged out successfully");
+					break;
+				case EXIT:
+					running = false;
+					break;
+				default:
+					break;
+			}
+		}
+		db.commit();
+		db.close();
+	}
+
+	private static String interpretCommand(String command) {
+		String toReturn = command;
+		switch (command) {
+			case "?":
+			case "h":
+				toReturn = "help";
+				break;
+			case "in":
+				toReturn = "login";
+				break;
+			case "out":
+				toReturn = "logout";
+				break;
+			case "x":
+				toReturn = "exit";
+				break;
+		}
+		return toReturn;
+	}
+
+	private FogUser user;
+
+	public DatabaseProject() {
+		super(null);
+
+		this.addCommand(new Login());
+		this.addCommand(new UserSignup());
+	}
+
 }
